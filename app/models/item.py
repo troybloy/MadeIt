ce0216edc1d3c7e1cad4e579f078100a99c95098
Troy Lee <troylee@berkeley.edu>
2023-03-22 17:52:10 -0700

    changing my models because postgres doesn't like them

--- app/models/item.py
+++ app/models/item.py
@@ -8,9 +8,9 @@ class Item(db.Model):
 
   id = db.Column(db.Integer, primary_key = True)
   owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable = False)
-  item_name = db.Column(db.String(50), nullable = False)
-  item_price = db.Column(db.String(10), nullable = False)
-  item_description = db.Column(db.String(255), nullable = False)
+  item_name = db.Column(db.String(100), nullable = False)
+  item_price = db.Column(db.Float, nullable = False)
+  item_description = db.Column(db.Text, nullable = False)
   item_img = db.Column(db.String(500))
   shop_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('shops.id')), nullable=False)
 
