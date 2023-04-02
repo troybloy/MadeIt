28871adfa4afb4ec54f77d4835e79fc1e2f7d40e
Troy Lee <troylee@berkeley.edu>
2023-04-01 21:19:04 -0700

    redid migration because I was missing some imports

--- migrations/versions/20230401.py
+++ migrations/versions/20230401.py
@@ -2,12 +2,15 @@
 
 Revision ID: ffdc0a98111c
 Revises:
-Create Date: 2020-11-20 15:06:02.230689
+Create Date: 2024-04-01 19:36:42.254389
 
 """
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '594e0d295c26'
@@ -50,6 +53,10 @@ def upgrade():
     sa.ForeignKeyConstraint(['shop_id'], ['shops.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE shops SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE items SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
